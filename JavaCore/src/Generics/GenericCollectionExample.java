7d6d272078ba652cb38dbd78196017c2a3628a6e
stevetai <stevetaidev@gmail.com>
2023-11-27 23:01:08 +0700

    First Program with Generics

--- JavaCore/src/Generics/GenericCollectionExample.java
+++ JavaCore/src/Generics/GenericCollectionExample.java
@@ -1,4 +1,17 @@
 package Generics;
+import java.util.ArrayList;
 
 public class GenericCollectionExample {
+    public static void main(String[] args) {
+        ArrayList<String> listString = new ArrayList<>();
+
+        // Thêm hai chuỗi vào listString sử dụng phương thức add() của ArrayList.
+        listString.add("This is 1st string");
+        listString.add("This is 2nd string");
+
+        // Sử dụng vòng lặp for-each để lặp qua các phần tử trong listString.
+        for (String newlistString : listString) {
+            System.out.println(newlistString);
+        }
+    }
 }
