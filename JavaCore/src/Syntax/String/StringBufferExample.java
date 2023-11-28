3087a43a36a33661367dd2769f75491a8ff8ef9b
stevetai <stevetaidev@gmail.com>
2023-11-28 11:30:38 +0700

    String Buffer Example

--- JavaCore/src/Syntax/String/StringBufferExample.java
+++ JavaCore/src/Syntax/String/StringBufferExample.java
@@ -1,19 +1,16 @@
 package Syntax.String;
 
-import Generics.List;
-
 public class StringBufferExample {
     public static void main(String[] args) {
         StringBuffer sb = new StringBuffer(); // Khởi tạo một StringBuffer rỗng
 
         sb.append("I Love" + " " + "Java"); // Thêm chuỗi "Hello" vào sb
-        sb.append(" ");     // Thêm khoảng trắng vào sb
+        sb.append(" ");     //
         sb.append(" || I Love C# "); //
 
 
         String result = sb.toString(); // Chuyển StringBuffer thành String
         System.out.println(result);
-
 
     }
 }
