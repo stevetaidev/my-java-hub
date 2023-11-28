1a9d1fc735070a12e091bb9fc7da0e456abe2488
stevetai <stevetaidev@gmail.com>
2023-11-28 22:01:46 +0700

    Inheritance Demo Edited

--- JavaCore/src/Syntax/OOP/Inheritance/Main.java
+++ JavaCore/src/Syntax/OOP/Inheritance/Main.java
@@ -3,13 +3,14 @@ package Syntax.OOP.Inheritance;
 public class Main {
     public static void main(String[] args) {
         Dog myDog = new Dog("Buddy");
+        Dog myDog2 = new Dog("Buddy2");
 
         // Gọi phương thức từ lớp cha
         myDog.eat();
         myDog.sleep();
 
         // Gọi phương thức của lớp con
-        myDog.bark();
+        myDog2.bark();
     }
 }
 
